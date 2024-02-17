7876700d25ddc859208686d293482be43ba3ba5e
thigam <matthewthiga72@gmail.com>
2024-02-17 21:25:38 +0100

    First push for 102

---
+++ 102-counting_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+
+/**
+ * void counting_sort- Sorts an array of integers in ascending order using the Counting sort algorithm
+ * @array: The array to be sorted
+ * @size: the size/ length of the array
+ *
+ * Return: nothing
+ */
+
+void counting_sort(int *array, size_t size)
+{
+
